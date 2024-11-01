002a5aa0c160c0353ae3edc7e2e26dc94255e989
Venkataramana <venkychinna297@gmail.com>
2024-11-01 20:28:45 +0530

    Loading functionality is done

--- mini-project/src/components/Loading/index.js
+++ mini-project/src/components/Loading/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import {TailSpin} from 'react-loader-spinner'
 
-const Loading = () => {
-  return (
-    <div>
-      This is Loading
-    </div>
-  )
-}
+import './index.css'
+
+const Loading = () => (
+  <div className="loader-container" data-testid="loader">
+    <TailSpin type="TailSpin" color="#D81F26" height="50" width="50" />
+  </div>
+)
 
 export default Loading
