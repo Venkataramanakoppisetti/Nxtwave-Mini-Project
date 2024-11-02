2f0c0f29b2bb31f1785b95e89cc7f8526b345680
Venkataramana <venkychinna297@gmail.com>
2024-11-02 17:57:43 +0530

    Account section completed

--- mini-project/src/components/Account/index.js
+++ mini-project/src/components/Account/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 import Header from '../Header';
+import Footer from '../Footer';
+
+import './index.css';
 
 const Account = () => {
+  const navigate = useNavigate();
+
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token');
+    navigate('/login');
+  };
+
   return (
     <>
+      <div className="header-sec">
         <Header />
-        <div>
-            <h1>This is Account</h1>
+      </div>
+      <div className="account-container">
+        <h1 className="heading">Account</h1>
+        <hr className="hz-line" />
+        <div className="membership-container">
+          <p className="member-details">Membership :</p>
+          <div className="user-details">
+            <p className="username">venkychinna297@gmail.com</p>
+            <p className="password">Password : **********</p>
+          </div>
         </div>
+        <hr className="hz-line" />
+        <div className="plan-details-container">
+          <p className="member-details">Plan Details :</p>
+          <p className="username">Premium</p>
+          <p className="plan-type">Ultra HD</p>
+        </div>
+        <hr className="hz-line" />
+        <button onClick={onClickLogout} type="button" className="logout-btn">
+          Logout
+        </button>
+      </div>
+      <div className="footer-sec">
+        <Footer />
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
